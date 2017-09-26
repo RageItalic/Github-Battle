4db7c3fc8b1b1b9d32f18459df60bbda753fe3db
RageItalic <parthpatelgee@gmail.com>
2017-09-26 01:12:52 -0400

    app fully functional. need to make components more reusable and refactor the code next

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
+var PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
 	constructor(props) {
@@ -41,14 +43,14 @@ class PlayerInput extends React.Component {
 					id="username" 
 					placeholder="github username" 
 					type="text" 
-					autocomplete="off" 
+					autoComplete="off" 
 					value={this.state.username} 
 					onChange={this.handleChange}
 				/>
 				<button 
 					className="button" 
 					type="submit" 
-					disable={!this.state.username}>
+					disabled={!this.state.username}>
 					Submit
 				</button>
 			</form>
@@ -77,6 +79,7 @@ class Battle extends React.Component {
 		};
 		
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleReset = this.handleReset.bind(this);
 	}
 
 	handleSubmit(id, username) {
@@ -88,9 +91,21 @@ class Battle extends React.Component {
 		})
 	}
 
+	handleReset(id) {
+		this.setState(function(){
+			var newState = {};
+			newState[id + 'Name'] = '';
+			newState[id + 'Image'] = null;
+			return newState;
+		})
+	}
+
 	render () {
+		var match = this.props.match;
 		var playerOneName = this.state.playerOneName;
 		var playerTwoName = this.state.playerTwoName;
+		var playerOneImage = this.state.playerOneImage;
+		var playerTwoImage = this.state.playerTwoImage;
 		return (
 			<div>
 				<div className='row'>
@@ -101,6 +116,15 @@ class Battle extends React.Component {
 							onSubmit={this.handleSubmit} 
 						/>
 					}
+					{playerOneImage !== null && 
+						<PlayerPreview 
+							avatar={playerOneImage}
+							username={playerOneName}
+						>
+							<button className="reset" onClick={this.handleReset.bind(null, 'playerOne')}>Reset</button>
+						</PlayerPreview>
+					}
+
 					{!playerTwoName && 
 						<PlayerInput 
 							id="playerTwo" 
@@ -108,7 +132,23 @@ class Battle extends React.Component {
 							onSubmit={this.handleSubmit} 
 						/>
 					}
+					{playerTwoImage !== null && 
+						<PlayerPreview 
+							avatar={playerTwoImage}
+							username={playerTwoName}
+						>
+							<button className="reset" onClick={this.handleReset.bind(null, 'playerTwo')}>Reset</button>
+						</PlayerPreview>
+					}
 				</div>
+				{playerOneImage && playerTwoImage &&
+					<Link className="button" to={{
+						pathname: match.url + '/results',
+						search: `?playerOneName=` + playerOneName + '&playerTwoName=' + playerTwoName
+					}}>
+						Battle
+					</Link>
+				}
 			</div>
 		)
 	}
