e9f5307caf57fef7bd47fc8560f34ae64bd3652e
RageItalic <parthpatelgee@gmail.com>
2017-09-28 14:08:37 -0400

    made Loading a highly reusable component

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,9 +1,10 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Loading = require('./Loading');
 
 function SelectLanguage(props) {
-	var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python', 'C++', 'Elixir'];
+	var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python', 'C++', 'Elixir', 'Erlang'];
 
 	return (
 		<ul className="languages">
@@ -97,7 +98,7 @@ class Popular extends React.Component {
 					selectedLanguage={this.state.selectedLanguage} 
 					onSelect={this.updateLanguage} 
 				/>
-				{!this.state.repos ? <p>Loading...</p> : <RepoGrid repos={this.state.repos}/> }
+				{!this.state.repos ? <Loading /> : <RepoGrid repos={this.state.repos}/> }
 			</div>
 		)
 	}
